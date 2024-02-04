55cc35e997274adf18333eb096e542cf83c59d30
metalheadvh <148365087+metalheadvh@users.noreply.github.com>
2024-02-05 01:28:57 +0200

    all and negative amounts

--- bank-hw.js
+++ bank-hw.js
@@ -1,6 +1,11 @@
 const accountCharacters = ["RO", 1, 2, 3, 4, 5, "A", "B", "C", "D", "E", "F"];
+<<<<<<< Updated upstream
 const sums = [-1000, -2000, 3000, 4000];
 let amount = sums[Math.floor(Math.random() * sums.length)];
+=======
+const sums = ["-1000", "-2000", "3000", "4000"];
+//let amount = sums[Math.floor(Math.random() * sums.length)];
+>>>>>>> Stashed changes
 
 class bancaLuMatei {
   constructor(name, swift, country) {
@@ -26,9 +31,34 @@ class bancaLuMatei {
     }
     return password;
   }
+<<<<<<< Updated upstream
   displayAllAccounts() {
     for (let i = 0; i < bank.accounts.length; i++) {
       console.log(bank.accounts[i]);
+=======
+
+  //random account - change form while to for
+  generateRandomSum(length) {
+    let password = "";
+    for (let i = 0; i < length; i++) {
+      const random = sums[Math.floor(Math.random() * sums.length)];
+      password = password + random;
+    }
+    return password;
+  }
+
+  displayAllNegativeAccounts() {
+    for (let i = 0; i < this.accounts.length; i++) {
+      const cont = this.accounts[i];
+      if (cont.sum < 0) {
+        console.log(this.accounts[i]);
+      }
+    }
+  }
+  displayAllAccounts() {
+    for (let i = 0; i < this.accounts.length; i++) {
+      console.log(this.accounts[i]);
+>>>>>>> Stashed changes
     }
   }
   displayAccount(parameter) {
@@ -45,6 +75,13 @@ let account3 = bank.adaugaCont(`${bank.generateRandomAccount(20)}`, amount);
 const accountNumber = [account1, account2, account3];
 
 bank.displayAllAccounts();
+<<<<<<< Updated upstream
 bank.displayAccount(0);
 bank.displayAccount(1);
 bank.displayAccount(2);
+=======
+bank.displayAllNegativeAccounts();
+// bank.displayAccount(0);
+// bank.displayAccount(1);
+// bank.displayAccount(2);
+>>>>>>> Stashed changes
