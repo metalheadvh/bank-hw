5fc1c807973a2ac9a260fc564d06c915b74db417
Matei Dascalu <dascalumatei@gmail.com>
2024-02-04 22:12:52 +0200

    fixing the automatic and random bank balance generator

--- bank-hw.js
+++ bank-hw.js
@@ -1,50 +1,61 @@
 const accountCharacters = ["RO", 1, 2, 3, 4, 5, "A", "B", "C", "D", "E", "F"];
 const sums = [-1000, -2000, 3000, 4000];
-let amount = sums[Math.floor(Math.random() * sums.length)];
+//let amount = sums[Math.floor(Math.random() * sums.length)];
 
 class bancaLuMatei {
-  constructor(name, swift, country) {
-    this.name = name;
-    this.swift = swift;
-    this.country = country;
-    this.accounts = [];
-  }
+    constructor(name, swift, country) {
+        this.name = name;
+        this.swift = swift;
+        this.country = country;
+        this.accounts = [];
+    }
 
-  adaugaCont(iban, sum) {
-    let account = {
-      iban: iban,
-      sum: sum,
-    };
-    this.accounts.push(account);
-    return account;
-  }
-  generateRandomAccount(length) {
-    let password = "";
-    while (password.length < length) {
-      const index = Math.ceil(Math.random() * accountCharacters.length - 1);
-      password = password + accountCharacters[index];
+    adaugaCont(iban, sum) {
+        let account = {
+            iban: iban,
+            sum: sum,
+        };
+        this.accounts.push(account);
+        return account;
     }
-    return password;
-  }
-  displayAllAccounts() {
-    for (let i = 0; i < bank.accounts.length; i++) {
-      console.log(bank.accounts[i]);
+    generateRandomAccount(length) {
+        let password = "";
+        while (password.length < length) {
+            const index = Math.ceil(Math.random() * accountCharacters.length - 1);
+            password = password + accountCharacters[index];
+        }
+        return password;
     }
-  }
-  displayAccount(parameter) {
-    console.log(accountNumber[parameter]);
-  }
+
+    //random account
+    generateRandomSum(length) {
+        let password = "";
+        while (password.length < length) {
+            const i = Math.ceil(Math.random() * sums.length - 1);
+            password = password + sums[i];
+        }
+        return password;
+    }
+
+    displayAllAccounts() {
+        for (let i = 0; i < bank.accounts.length; i++) {
+            console.log(bank.accounts[i]);
+        }
+    }
+    displayAccount(parameter) {
+        console.log(accountNumber[parameter]);
+    }
 }
 
 let bank = new bancaLuMatei();
 
-let account1 = bank.adaugaCont(`${bank.generateRandomAccount(20)}`, amount);
-let account2 = bank.adaugaCont(`${bank.generateRandomAccount(20)}`, amount);
-let account3 = bank.adaugaCont(`${bank.generateRandomAccount(20)}`, amount);
+let account1 = bank.adaugaCont(bank.generateRandomAccount(20), bank.generateRandomSum(1));
+let account2 = bank.adaugaCont(bank.generateRandomAccount(20), bank.generateRandomSum(1));
+let account3 = bank.adaugaCont(bank.generateRandomAccount(20), bank.generateRandomSum(1));
 
-const accountNumber = [account1, account2, account3];
+// const accountNumber = [account1, account2, account3];
 
 bank.displayAllAccounts();
-bank.displayAccount(0);
-bank.displayAccount(1);
-bank.displayAccount(2);
+// bank.displayAccount(0);
+// bank.displayAccount(1);
+// bank.displayAccount(2);
